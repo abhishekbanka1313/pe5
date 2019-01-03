8392a16472745cf21cefd2ddaa18db6c210ddb7b
abhishekbanka1313 <abhishekbanka1313@gmail.com>
2019-01-03 11:53:44 +0530

    All issues resolved

--- src/main/java/com/stackroute/unittest/pe5/ChangeArrayElement.java
+++ src/main/java/com/stackroute/unittest/pe5/ChangeArrayElement.java
@@ -5,21 +5,13 @@ import java.util.*;
 
 public class ChangeArrayElement {
 
-    public static void main(String[] args) {
+    public ArrayList<String> changingArrayElement(ArrayList<String> fruitsList) {
 
-
-            ArrayList<String> fruitsList = new ArrayList<String>(Arrays.asList("Apple", "Grape", "Melon", "Berry"));
-
-            System.out.println(fruitsList);
-
-            for (int i = 0; i < fruitsList.size(); i++) {
-                if (fruitsList.get(i).equalsIgnoreCase("Apple")) {
-                    fruitsList.set(i, "Kiwi");
-                }
+        for (int i = 0; i < fruitsList.size(); i++) {
+            if (fruitsList.get(i).equalsIgnoreCase("Apple")) {
+                fruitsList.set(i, "Kiwi");
             }
-
-            System.out.println(fruitsList);
-            fruitsList.clear();
-            System.out.println("The ArrayList after clearing is" + fruitsList);
         }
+        return fruitsList;
     }
+}
--- src/main/test/com/stackroute/unittest/pe5/ChangeArrayElementTest.java
+++ src/main/test/com/stackroute/unittest/pe5/ChangeArrayElementTest.java
@@ -4,6 +4,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class ChangeArrayElementTest {
 
     ChangeArrayElement obj;
@@ -19,9 +24,11 @@ public class ChangeArrayElementTest {
 
     @Test
     public void addRemoveArray() {
+        ArrayList<String> fruitsList = new ArrayList<String>(Arrays.asList("Apple", "Grape", "Melon", "Berry"));
+        ArrayList<String> changedFruitsList = new ArrayList<String>(Arrays.asList("Kiwi", "Grape", "Melon", "Berry"));
+
 //        ArrayList<String> fruitsList = new ArrayList<String>(Arrays.asList( "Apple","Grape","Melon","Berry") );
 //        ArrayList<String> resultList = new ArrayList<String>(Arrays.asList( "Kiwi","Grape","Melon","Berry"));
-//        assertEquals(fruitsList,obj.addRemoveArray(resultList));
-        System.out.println();
+        assertEquals(changedFruitsList,obj.changingArrayElement(fruitsList));
     }
 }
